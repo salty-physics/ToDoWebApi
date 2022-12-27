2772fdec512467703796729332c102bfb7e73f41
Michal Stachura <gritoo@gmail.com>
2022-12-27 20:33:48 +0100

    restart test

--- ToDoWebApi/Program.cs
+++ ToDoWebApi/Program.cs
@@ -10,7 +10,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching "
 };
 
 app.MapGet("/weatherforecast", () =>
